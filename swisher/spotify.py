294e60290673e6e657f50f5a5b7f99bfeefd72a8
Thomas Rynne <github@thomasrynne.co.uk>
2013-06-30 00:40:43 +0100

    Better help text and spotify uri lookup

--- swisher/spotify.py
+++ swisher/spotify.py
@@ -31,18 +31,20 @@ class SpotifyApi:
     def __init__(self):
         pass
 
+    def urilookup(self, uri):
+        return self.request("lookup", "", uri=uri)
     def track_search(self, text):
       if text != "":
-        for track in self.request("track", q=text)["tracks"]:
+        for track in self.request("search", "track", q=text)["tracks"]:
             uri = track["href"]
             name = track["name"]
             artist = track["artists"][0]["name"]
             value = { "spotify_uri":uri, "artist":artist, "track_name":name}
             yield (value, name, name + " ["+artist+"]")
             
-    def request(self, path, **params):
+    def request(self, service, path, **params):
         connection = httplib.HTTPConnection("ws.spotify.com")
-        url = "/search/1/"+path+".json" + "?" + "".join(
+        url = "/"+service+"/1/"+path+".json" + "?" + "".join(
           ["&"+name+"="+str(value).replace(" ", "+") for name, value in params.items()])
         print ( url )
         connection.request("GET", url)
@@ -64,11 +66,20 @@ class SpotifySearchPage:
     def index(self, search=""):
         uri = None
         tracks = None
-        if search.startswith("spotify:"):
+        if search.startswith("spotify:track"):
+           cleanuri = search
+           starttime = ""
+           hash = cleanuri.find("#")
+           if hash != -1:
+               cleanuri = search[:hash]
+               starttime = search[hash+1:]
+           track = self.spotifyapi.urilookup(cleanuri)["track"]
+           name = track["name"]
+           artist = track["artists"][0]["name"]
            uri = Entry()
-           uri.value = { "spotify_uri": search }
-           uri.name = search
-           uri.longname = search
+           uri.value = { "spotify_uri": search, "artist": artist, "track_name": name }
+           uri.name = name
+           uri.longname = name + " [" + artist + "] " + starttime
         else:
             tracks = self.spotifyapi.track_search(search)
         return self.context.render("spotify.html", "Spotify", search=search, uri=uri, tracks=tracks)
